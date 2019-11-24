8c73ceb221f1e9401617810c5dc5cd2fcc18d79a
Lal Shubham <khizaruddins@gmail.com>
2019-11-24 19:28:35 +0530

    added material input field on click change and did some tweaking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,18 @@ import { ChildComponent } from './parent/child/child.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
 	declarations: [ AppComponent, ParentComponent, ChildComponent ],
-	imports: [ BrowserModule, BrowserAnimationsModule, MatTableModule, MatInputModule, MatButtonModule ],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatTableModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule
+	],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
